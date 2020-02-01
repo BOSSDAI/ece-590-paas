c33f8d1b78f96ca4583522a9eca336aea84c6fc3
BSD <thebestpenholder@gmail.com>
2020-02-01 11:46:34 -0500

    two

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
+
+@app.route('/try')
+def my_try():
+    return "try"
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
